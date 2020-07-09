f9680b162c67770dc81eda976dc67ffa6b76a8a6
sebastianaclane <sebastianaclane@gmail.com>
2020-07-09 16:29:59 -0700

    Clean up and clarify comments in SongPicker component

--- src/components/SongPicker.js
+++ src/components/SongPicker.js
@@ -5,21 +5,15 @@ class SongPicker extends React.Component {
 
     removeSpaces = (event) => {
         if (event.currentTarget.value.includes(" ")) {
-            // /\s/g expression matches all empty spaces
+            // replace all spaces with empty characters
             event.currentTarget.value = event.currentTarget.value.replace(/\s/g, "");
         }
     }
 
+    // Get text from Song Name (songId) input and go to /song/songId page
     goToSongPlan = (event) => {
-        // 1. Stop the form from submitting
         event.preventDefault();
-        // 2. Get the text from the input and call it the songName
         const songId = this.songInput.current.value;
-        console.log(songId);
-        // Find a way to disable spaces inside a text input and maybe replace the removeSpaces function if it doesn't really work to be inside the helpers.js file
-        // Maybe https://stackoverflow.com/questions/14236873/disable-spaces-in-input-and-allow-back-arrow 
-        // or https://www.sitepoint.com/community/t/disabling-spaces-spacebar-in-a-form-textfield/5601/6
-        // 3. Change the page to song/songId
         this.props.history.push(`/song/${songId}`);
     };
     
