7bbdc40681d512576c855aa29db0976758c72550
sebastianaclane <sebastianaclane@gmail.com>
2020-07-14 13:29:21 -0700

    Replace normalize.css file and border-box css with bootstrap library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Router from './components/Router';
-import "normalize.css";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./css/style.css";
 
 render(
