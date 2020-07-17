07b6af2c674bcbf850031f1f4e69241528337a3c
sebastianaclane <sebastianaclane@gmail.com>
2020-07-17 16:32:03 -0700

    Change App.js into a React component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from "prop-types";
 import Header from "./Header";
 
-
-function App() {
-  return (
-    <div className="song-planner">
-      <Header title="Song Planner" />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="song-planner">
+        <Header title="Song Planner" />
+      </div>
+    )
+  }
 }
 
 export default App;
