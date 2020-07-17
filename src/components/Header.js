4eff4bf4b1be6aaab33b6304cec47d4c7e63ce33
sebastianaclane <sebastianaclane@gmail.com>
2020-07-17 16:12:51 -0700

    Add beamed eighth notes music emoji before "Song Planner" title

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ const Header = (props) => (
     <header className="container-fluid">        
             <ul class="nav">
                 <li class="nav-item">
-                    {/* Add a music symbol icon before the title */}
-                    <h1 className="title"><a class="nav-link active" href="#">{props.title}</a></h1>
+                    <h1 className="title"><a class="nav-link active" href="#">♫ {props.title}</a></h1>
                 </li>
             </ul>            
     </header>
